2704ddfaaeae6044120d3383684c3ae0a3779ff6
jmccallo <jeremy.mccallop@yahoo.com>
2023-06-30 08:10:11 -0400

    debugging code

--- service/routes.py
+++ service/routes.py
@@ -67,22 +67,15 @@ def list_accounts():
     List all accounts
     This endpoint will list all Accounts
     """
-
     app.logger.info("Request to list Accounts")
-
     # use the Account.all() method to retrieve all accounts
     accounts = Account.all()
-
     # create a list of serialize() accounts
     account_list = [account.serialize() for account in accounts]
-
     # log the number of accounts being returned in the list
     app.logger.info("Returning [%s] accounts", len(account_list))
-
     # return the list with a return code of status.HTTP_200_OK
     return jsonify(account_list), status.HTTP_200_OK
-
-
 
 ######################################################################
 # READ AN ACCOUNT
@@ -95,15 +88,12 @@ def get_accounts(account_id):
     Reads an Account
     This endpoint will read an Account based on the account_id that is requested
     """
-
     app.logger.info("Request to read an Account with id: %s", account_id)
     # use the Account.find() method to find the account
     account = Account.find(account_id)
-
     # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found 
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
     # return the serialize() version of the account with a return code of status.HTTP_200_OK
     return account.serialize(), status.HTTP_200_OK
 
@@ -119,20 +109,15 @@ def update_accounts(account_id):
     This endpoint will update an Account based on the posted data
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
-
     # use the Account.find() method ot retrieve the account by the account_id
     account = Account.find(account_id)
-
     # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
     # call the deserialize() method on the account passing in request.get_json()
     account.deserialize(request.get_json())
-
     # call account.update() to update the account with the new data
     account.update()
-
     # return the serialize() version of the account with a return code of status.HTTP_200_OK
     return account.serialize(), status.HTTP_200_OK
 
@@ -148,21 +133,17 @@ def delete_accounts(account_id):
     This endpoint will delete an Account based on the account_id that is requested
     """
     app.logger.info("Request to delete an Acount with id: %s", account_id)
-
     # use the Account.find() method to retrieve the account by the account_id\
     account = Account.find(account_id)
-
     # if found, call the delete() method on the account
     if account:
         account.delete()
-
     # return and empty body ("") with a return code of status.HTTP_204_NO_CONTENT
     return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
